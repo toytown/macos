d9f7f7257144682be6a9e7cdf458254b8da7cd30
Prasanna Tuladhar <tuladhp@amazon.com>
2023-01-05 17:24:36 +0100

    shortcuts for bufferline

--- nvim/lua/tuladhp/plugins/bufferline.lua
+++ nvim/lua/tuladhp/plugins/bufferline.lua
@@ -2,3 +2,5 @@ require("bufferline").setup({})
 
 vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
 vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
+vim.keymap.set("n", "<leader>xR", "<Cmd>BufferLineCloseRight<CR>", {})
+vim.keymap.set("n", "<leader>xL", "<Cmd>BufferLineCloseLeft<CR>", {})
