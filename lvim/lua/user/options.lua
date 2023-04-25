0576b9626af52b30de0e1726eb4a15d9f4c92e5e
Prasanna Tuladhar <tuladhp@amazon.com>
2023-04-25 08:44:32 +0200

    added lua options for neovim 0.9

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,8 +1,8 @@
+local dap = require("lvim.core.dap")
 vim.opt.guicursor = ""
 vim.opt.scrolloff = 8
-vim.g.tokyonight_dark_sidebar = true
 
-lvim.log.level = "warn"
+vim.log.level = "warn"
 
 -- disable up/down arrow keys (customizations)
 vim.keymap.set("n", "<Left>", "<nop>")
@@ -26,10 +26,8 @@ lvim.keys.normal_mode["<S-Tab>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
 
 -- toggleterm (customization)
-lvim.builtin.terminal.direction = "horizontal"
+lvim.builtin.terminal.direction = "float"
 lvim.builtin.terminal.open_mapping = "<C-t>"
-lvim.builtin.terminal.shade_terminals = false
-lvim.builtin.terminal.shading_factor = 1
 
 -- -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
@@ -38,45 +36,135 @@ lvim.builtin.terminal.shading_factor = 1
 -- -- Change theme settings
 lvim.colorscheme = "tokyonight-night"
 
--- After changing plugin config exit and reopen LunarVim, Run :PackerSync
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-
--- customizations
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+-- customizationsi nvim tree
 lvim.builtin.nvimtree.setup.git.ignore = true
+lvim.builtin.nvimtree.setup.actions.change_dir.restrict_above_cwd = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+lvim.builtin.nvimtree.setup.filters.custom = {}
+
+-- treesitter
+lvim.builtin.treesitter.ignore_install = {}
+lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.ensure_installed = {
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"tsx",
+	"css",
+	"rust",
+	"java",
+	"yaml",
+}
+
+-- project
+lvim.builtin.project.detection_methods = { "lsp", "pattern" }
+lvim.builtin.project.patterns = {
+	".git",
+	"package-lock.json",
+	"yarn.lock",
+	"package.json",
+	"requirements.txt",
+}
+
+-- telescope
+lvim.builtin.telescope.defaults.path_display = {
+	shorten = 4,
+}
 
 -- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-  { command = "stylua" },
-  {
-    command = "prettier",
-    extra_args = { "--print-width", "120" },
-    filetypes = { "typescript", "typescriptreact" },
-  },
+	{ command = "stylua" },
+	{
+		command = "prettier",
+		extra_args = { "--print-width", "120" },
+		filetypes = { "typescript", "typescriptreact" },
+	},
 })
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
-  { command = "flake8", filetypes = { "python" } },
-  { command = "eslint", filetypes = { "typescript", "typescriptreact" } },
-  {
-    command = "shellcheck",
-    args = { "--severity", "warning" },
-  },
+	{ command = "flake8", filetypes = { "python" } },
+	{ command = "eslint", filetypes = { "typescript", "typescriptreact", "json" } },
+	{
+		command = "shellcheck",
+		args = { "--severity", "warning" },
+	},
 })
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{
+		"tpope/vim-fugitive",
+		cmd = {
+			"G",
+			"Git",
+			"Gdiffsplit",
+			"Gread",
+			"Gwrite",
+			"Ggrep",
+			"GMove",
+			"GDelete",
+			"GBrowse",
+			"GRemove",
+			"GRename",
+			"Glgrep",
+			"Gedit",
+		},
+		ft = { "fugitive" },
+	},
+	{
+		"tpope/vim-surround",
+
+		-- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+		-- setup = function()
+		--  vim.o.timeoutlen = 500
+		-- end
+	},
+	{ "tpope/vim-repeat" },
+	{
+		"sindrets/diffview.nvim",
+		event = "BufRead",
+	},
+	{
+		"nvim-telescope/telescope-frecency.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "kkharji/sqlite.lua" },
+	},
+	{
+		"AckslD/nvim-neoclip.lua",
+		config = function()
+			require("neoclip").setup()
+		end,
+	},
+	{
+		"phaazon/hop.nvim",
+		branch = "v2",
+		config = function()
+			require("hop").setup()
+		end,
+	},
 }
+
+lvim.builtin.telescope.on_config_done = function(telescope)
+	pcall(telescope.load_extension, "frecency")
+	pcall(telescope.load_extension, "neoclip")
+end
+
+-- keymaps
 lvim.keys.normal_mode["<leader>tt"] = ":TroubleToggle<CR>"
 lvim.keys.normal_mode["<leader>tw"] = ":TroubleToggle workspace_diagnostics<CR>"
 lvim.keys.normal_mode["<leader>td"] = ":TroubleToggle document_diagnostics<CR>"
 lvim.keys.normal_mode["<leader>tl"] = ":TroubleToggle loclist<CR>"
 lvim.keys.normal_mode["<leader>tq"] = ":TroubleToggle quickfix<CR>"
+
+lvim.keys.normal_mode["<leader>y"] = ":Telescope neoclip default<CR>"
+
+lvim.keys.insert_mode["jk"] = "<ESC>"
